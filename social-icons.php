d83b6cc4cddea606b941769761f8c8e0e030be85
Tony Edwards <tony.edwards@gmail.com>
2015-01-15 23:31:36 +0000

    107 - Font sizes for plugin
    Added font sizes for the 'Font Size Adjust' plugin.

--- social-icons.php
+++ social-icons.php
@@ -4,14 +4,17 @@
 $twitter = get_option('path_twitter');
 $easyFundraising = get_option('path_easy_fundraising');
 ?>
-
+	
 	<ul>
+		<?php wp_fontsize::html(); ?>
+		
 		<?php if($twitter): ?>
 	    <li class="twitter social">
 	        <a href="<?php print $twitter; ?>" title="Find PATH on Twitter" target="_blank">
 	            <i class="social_icon fa fa-twitter"></i></a>
 	    </li>
-
+	    <?php endif; ?>
+	    <?php if($easyFundraising): ?>
 	    <li class="easyFundraising social">
 	    	<a href="<?php print $easyFundraising; ?>" title="Easy Fundraising" target="_blank">
 	    	ef</a>
